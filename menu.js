65efcaaead00506b36e735c7dbba1e883a082319
nickybritt <162335065+nickybritt@users.noreply.github.com>
2025-06-29 06:07:27 -0700

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('nav ul');
+const menuBtn = document.querySelector('.menu-open');
+const closeBtn = document.querySelector('.menu-close');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.add('open')
+})
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('open')
+})
